b7f64634574c002488eec0d7f51e213e4e66a841
devkodama <33792590+devkodama@users.noreply.github.com>
2019-04-23 05:01:02 -0500

    test

--- snake.js
+++ snake.js
@@ -866,6 +866,8 @@ class Controller {
       dx = playerSnake.body[0].pos.x - this.touchx / GRID_SCALE;
       dy = playerSnake.body[0].pos.y - this.touchy / GRID_SCALE;
 
+      return DOWN;
+
       if (Math.abs(dx) > Math.abs(dy)) {
         if (dx > 0) {
           return RIGHT;
@@ -1030,6 +1032,13 @@ function initializeGame() {
     if (gameCanvas.status == PLAY) {
     //  e.preventDefault();
     }
+    {
+      let ctx = gameCanvas.context;
+
+      ctx.font = "12 pt Arial";
+      ctx.fillStyle = "orange";
+      ctx.fillText( 'touchx ' + gameController.touchx + 'touchy ' + gameController.touchy, gameController.touchx, gameController.touchy);
+    }
 
   });
   window.addEventListener('touchmove', function (e) {
@@ -1038,7 +1047,7 @@ function initializeGame() {
     gameController.touchx = e.touches[0].screenX;
     gameController.touchy = e.touches[0].screenY;
     if (gameCanvas.status == PLAY) {
-    //  e.preventDefault();
+      e.preventDefault();
     }
     {
       let ctx = gameCanvas.context;
